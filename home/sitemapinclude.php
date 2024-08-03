ddd882ad19a5af36b410ef52b4c28de0ed13b719
Cristan Meijer <cristan.meijer@gmail.com>
2024-08-03 12:53:27 +0200

    Set release to 2024-1

--- home/sitemapinclude.php
+++ home/sitemapinclude.php
@@ -1,9 +1,9 @@
 <?php
 
 // lastmod format is YYYY-MM-DD
-$homeLastMod = "2024-06-27";
-$countryLastMod = "2024-06-29";
-$unlocodeLastMod = "2024-06-27";
+$homeLastMod = "2024-08-03";
+$countryLastMod = "2024-08-03";
+$unlocodeLastMod = "2024-08-03";
 
 $numResultsPerSitemap = 1000;
 ?>
--- include.php
+++ include.php
@@ -5,5 +5,5 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 
-$unlocodeVersion = 'UN/LOCODE 2023.2';
+$unlocodeVersion = 'UN/LOCODE 2024-1';
 ?>
